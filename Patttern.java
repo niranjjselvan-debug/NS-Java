9bb5b018c545763b14cb707beaa63c60bfb4564c
Niranjj Selvan <niranjjselvan1995@gmail.com>
2025-05-28 22:52:22 +0530

    Pattern Generator

---
+++ Patttern.java
@@ -0,0 +1,30 @@
+public class Patttern {
+    public static void main(String[] args) {
+        rightAngle();
+        centeredTriangle();
+    }
+
+    public static void rightAngle() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void centeredTriangle() {
+        int n = 5; // height of the triangle
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
