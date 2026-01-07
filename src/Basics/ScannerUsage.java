f6bf25e4a719448b5f9a02554dfa5cc0652e29e8
Niranjj Selvan <niranjjselvan@NS-MacBook-Pro.local>
2026-01-07 17:46:45 +0530

    Scannerusage demo

--- src/Basics/ScannerUsage.java
+++ src/Basics/ScannerUsage.java
@@ -1,7 +1,25 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class ScannerUsage {
     static void main(String[] args) {
+        Scanner scan = new Scanner(" Niranjj, Nivejaa, Geetha, Selvan");
+        scan.useDelimiter(",");
+        while (scan.hasNext()) {
+            System.out.println(scan.next());
+        }
+        scan.close();
 
+        Scanner scan_01 = new Scanner(System.in);
+        System.out.println("Enter an Integer : ");
+        int input_int = scan_01.nextInt();
+        System.out.println("Enter a string : ");
+        String input_string = scan_01.next();
+        scan_01.nextLine();
+        System.out.println("Enter a line : ");
+        String input_line = scan_01.nextLine();
+        System.out.println("Integer is : " + input_int + "String is :" + input_string + "Line is :" + input_line);
+        scan_01.close();
     }
 }
