37d05ae86de595f8fdc68d29ffec60ea1b7fc3a5
Niranjj Selvan <niranjjselvan@NS-MacBook-Pro.local>
2026-01-08 20:38:43 +0530

    Anagram using hashmap

---
+++ src/ProblemSolving/Anagram.java
@@ -0,0 +1,24 @@
+package ProblemSolving;
+
+import java.util.*;
+
+public class Anagram {
+    static void main(String[] args) {
+        List<String> words = Arrays.asList("tea", "tan", "eat", "ate", "nat", "bat");
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : words) {
+            char[] input = word.toCharArray();
+            Arrays.sort(input);
+            String key = new String(input);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+
+        for (List<String> group : map.values()) {
+            System.out.println(group);
+        }
+    }
+}
