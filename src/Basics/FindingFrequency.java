b9bd61f99917d115e6c39da0a83300bd9bf87471
Niranjj Selvan <niranjjselvan@NS-MacBook-Pro.local>
2026-01-09 11:11:50 +0530

    Finding frequency using hash map

---
+++ src/Basics/FindingFrequency.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindingFrequency {
+    static void main(String[] args) {
+        int[] input_int = {2, 3, 4, 5, 2, 4, 6, 2, 7};
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : input_int) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
