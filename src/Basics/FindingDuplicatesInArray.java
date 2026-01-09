b814dd4552df6214336aa827b46f958ff679c6d0
Niranjj Selvan <niranjjselvan@NS-MacBook-Pro.local>
2026-01-09 11:02:50 +0530

    Finding Duplicate in a array

---
+++ src/Basics/FindingDuplicatesInArray.java
@@ -0,0 +1,16 @@
+package Basics;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindingDuplicatesInArray {
+    static void main(String[] args) {
+        int[] input = {2, 3, 4, 2, 5, 3, 6};
+        Set<Integer> set = new HashSet<>();
+
+        for (int n : input) {
+            if (!set.add(n))
+                System.out.println("Duplicate:" + n);
+        }
+    }
+}
